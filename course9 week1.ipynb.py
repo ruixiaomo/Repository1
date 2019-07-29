edb008cf8aca011a54b69db20cace252531b3fc0
ruixiaomo <53415378+ruixiaomo@users.noreply.github.com>
2019-07-29 11:00:02 +0800

    Add files via upload

---
+++ course9 week1.ipynb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the notebook for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
